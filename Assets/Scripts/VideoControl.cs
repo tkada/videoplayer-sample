6ba72147601a52b452c8d989781debdaefe55966
Takeshi Kada <t.kada.1985@gmail.com>
2022-11-26 16:55:01 +0900

    Comment

--- Assets/Scripts/VideoControl.cs
+++ Assets/Scripts/VideoControl.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
@@ -19,23 +19,38 @@ public class VideoControl : MonoBehaviour
     {
         this.playButton.onClick.AddListener(() =>
         {
+            //再生する
+            //一時停止中の場合は再開する
             this.videoPlayer.Play();
         });
 
         this.stopButton.onClick.AddListener(() =>
         {
+            //停止する
             this.videoPlayer.Stop();
         });
 
         this.pauseButton.onClick.AddListener(() =>
         {
+            //一時停止する
             this.videoPlayer.Pause();
+        });
+
+        this.slider.onValueChanged.AddListener(value =>
+        {
+            //スライダーの場所に合わせて再生する場所を変える
+            this.videoPlayer.time = this.videoPlayer.length * value;
         });
     }
 
     void Update()
     {
-        this.slider.value = (float)(this.videoPlayer.time / this.videoPlayer.length);
+        //スライダーを再生時間に合わせて進行させる
+        //SetValueWithoutNotifyを使うとonValueChangedのコールバックが呼ばれない
+        //→UIで操作したイベントだけを取得することができる
+        this.slider.SetValueWithoutNotify((float)(this.videoPlayer.time / this.videoPlayer.length));
+
+        //現在再生時間を表示
         this.timeText.text = $"{(int)this.videoPlayer.time / 60:D2}:{(int)this.videoPlayer.time % 60:D2}";
     }
 }
